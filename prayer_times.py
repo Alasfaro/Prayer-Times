6da3288b7caca0c78b7d6ba76b6b834d5786fbaa
Omar Al-Asfar <122765463+Alasfaro@users.noreply.github.com>
2024-03-03 21:24:00 -0500

    Add files via upload

---
+++ prayer_times.py
@@ -0,0 +1,57 @@
+import requests
+from datetime import datetime, timedelta
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def get_prayer_times(lat, long):
+    base_url = f"http://api.aladhan.com/v1/timingsByCity?city=Mississauga&country=Canada&method=2"
+    response = requests.get(base_url)
+    data = response.json()
+    return data
+
+def prayer_times_update():
+    # Mississauga latitude and longitude
+    lat = 43.5890
+    long = -79.6441
+    prayer_times_data = get_prayer_times(lat, long)
+
+    timings = prayer_times_data['data']['timings']
+
+    Fajr = timings['Fajr']
+    Dhuhr = timings['Dhuhr']
+    Asr = timings['Asr']
+    Maghrib = timings['Maghrib']
+    Isha = timings['Isha']
+    midnight = get_midnight(Isha, Fajr)
+
+    prayer_times_info = (
+        f"Prayer Times in Mississauga:\n"
+        f"Fajr: {convert_to_12hour(Fajr)}\n"
+        f"Dhuhr: {convert_to_12hour(Dhuhr)}\n"
+        f"Asr: {convert_to_12hour(Asr)}\n"
+        f"Maghrib: {convert_to_12hour(Maghrib)}\n"
+        f"Isha: {convert_to_12hour(Isha)}\n"
+        f"Midnight: {midnight}\n"
+    )
+
+    return render_template('prayer_time.html', prayer_times_info)
+
+def get_midnight(Isha, Fajr):
+    isha_dt = datetime.strptime(Isha, '%H:%M')
+    fajr_dt = datetime.strptime(Fajr, '%H:%M')
+    diff = fajr_dt - isha_dt
+    if diff.days < 0:
+        diff += timedelta(days=1)
+    midnight_dt = isha_dt + diff/2
+    midnight = midnight_dt.strftime('%I:%M %p')
+    return midnight
+
+def convert_to_12hour(time):
+    time_24 = datetime.strptime(time, '%H:%M')
+    return time_24.strftime('%I:%M %p')
+
+prayer_times_update()
+
+if __name__ == '__main__':
+    app.run(debug=True)
